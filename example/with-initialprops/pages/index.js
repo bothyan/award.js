be53e88fa96493167b3398fed9feb0b290e81bdd
topthinking <topthinking@ximalaya.com>
2017-12-05 18:53:22 +0800

    修改了列子

--- example/with-initialprops/pages/index.js
+++ example/with-initialprops/pages/index.js
@@ -2,12 +2,13 @@
 import React from 'react'
 import { Link } from 'swrn/router'
 import axios from 'axios'
+import '../style/index.scss'
 
 export default class Index extends React.Component { 
 
     static async getInitialProps() {
         
-        const { data: { list } } = await axios.get('http://localhost:4000/api/list')
+        const { data: { list } } = await axios.get('http://localhost:3000/api/list')
 
         return {
             name: 'top',
@@ -31,10 +32,10 @@ export default class Index extends React.Component {
     render() { 
         return (
             <div>
-            <h1 className="hello" onClick={this.change.bind(this)} style={{ color: this.state.new ? 'red' : 'black' }}>
+            <h1 styleName="hello" onClick={this.change.bind(this)}>
                 hello {this.props.name}      
             </h1>
-                <Link to="/mine/12/home/140" tag="span"><a>mine</a></Link>
+                <Link to="/mine/12/home/140" tag="span">mine</Link>
                 <ul>{this.props.list.map(item => { 
                     return <li key={item.id}>{item.name}</li>
                 })}</ul>    
