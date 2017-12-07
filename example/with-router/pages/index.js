a2b5cd3e6ed5828bca3d71343685bcc549182b2d
topthinking <topthinking@ximalaya.com>
2017-12-07 11:43:38 +0800

    修复link的tag错误问题

--- example/with-router/pages/index.js
+++ example/with-router/pages/index.js
@@ -29,7 +29,29 @@ export default class Index extends React.Component {
             <h1 className="hello" onClick={this.change.bind(this)} style={{ color: this.state.new ? 'red' : 'black' }}>
                 hello {this.props.name}      
             </h1>
-                <Link to="/about/12/home/14" tag="span">查看详情12</Link>                  
+                <Link to="/about/12/home/14" tag="a">
+                    <div styleName="hello world">
+                        查看详情12
+                        <span>123</span>
+                        <p>321</p>
+                    </div>
+                </Link>  
+                
+                <Link to="/about/12/home/14">
+                    <div styleName="hello world">
+                        查看详情12
+                        <span>123</span>
+                        <p>321</p>
+                    </div>
+                </Link> 
+
+                <Link to="/about/12/home/14" className="hello world">
+                    123
+                </Link> 
+                
+                <Link to="/about/12/home/14">
+                    <img className="name"src="http://s1.xmcdn.com/apk/yx/dazhuzhai/img/banner.jpg"/>
+                </Link> 
             </div>    
         )
     }
